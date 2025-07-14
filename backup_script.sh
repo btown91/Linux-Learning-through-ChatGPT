5e71654907151e888060b43396482fd809e16041
btown91 <btown1775@gmail.com>
2025-07-14 16:35:50 -0700

    Comments added
    Comments added to each line to help explain what is happening.

--- backup_script.sh
+++ backup_script.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-SRC="$HOME/linux_practice"
-DEST="$HOME/backups/backup_$(date +%Y%m%d)"
+SRC="$HOME/linux_practice" # Where the backup data comes from
+DEST="$HOME/backups/backup_$(date +%Y%m%d)" # Where the backup will be stored (with today's date)
 
-mkdir -p "$DEST"
-cp -r "$SRC"/* "$DEST"
+mkdir -p "$DEST"# Creates the destination folder if it doesn't already exist
+cp -r "$SRC"/* "$DEST" # Copies all files from the source to the destination folder
 
-echo "Backup done at $(date) for $DEST" >> ~/backups/backup_log.txt
-echo "Backup completed"
+
+echo "Backup done at $(date) for $DEST" >> ~/backups/backup_log.txt # Logs the backup event with timestamp
+echo "Backup completed" # Prints confirmation to the screen
+
 
